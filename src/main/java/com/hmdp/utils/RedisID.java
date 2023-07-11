1813fbf7e190a268727ecbc64d97980c3fe7750b
zrcvae <2267085629@qq.com>
2023-07-11 23:17:41 +0800

    生成Reids自增全局ID

---
+++ src/main/java/com/hmdp/utils/RedisID.java
@@ -0,0 +1,43 @@
+package com.hmdp.utils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author zrc
+ * @date 2023/07/11
+ * 还可以尝试采用雪花算法写ID
+ */
+@Component
+public class RedisID {
+    /**
+     * 开始时间戳（从2022/1/1 0：0：0开始）
+     */
+    private static final long BEGIN_TIMESTAMP = 1640995200L;
+    /**
+     * 序列号位数
+     */
+    private static final int COUNT_BITS = 32;
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    public long nextId(String keyPrefix){
+        // 生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowS = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowS - BEGIN_TIMESTAMP;
+
+        // 生成序列号
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        long count = redisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        // 将时间戳和序列号拼接，生成全局ID
+        return timestamp << COUNT_BITS | count;
+    }
+
+}
--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,17 +1,29 @@
 package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisID;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class HmDianPingApplicationTests {
     @Autowired
     private ShopServiceImpl shopService;
+    @Autowired
+    private RedisID redisID;
     @Test
     void testSaveShop(){
         shopService.saveShopToRedis(1L, 10L);
     }
+
+    @Test
+    void testRedisId(){
+        long orderId = redisID.nextId("order");
+        System.out.println(orderId);
+    }
+
 
 }
